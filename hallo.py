8897390b9440e2557f95f78e034feb064fb091e7
obaid <obaidaqurea321@gmail.com>
2021-12-06 12:13:21 +0100

    eerste commit!

--- hallo.py
+++ hallo.py
@@ -1,1 +1,10 @@
+print ("hello wereld van…")                                    
 
+print ("pppp    y     y   ttttttt   h   h    oooo   nnnn")          
+print ("p   p    y   y       t      h   h   o    o  n   n")
+print ("p   p     y y        t      h   h   o    o  n   n")
+print ("pppp       y         t      hhhhh   o    o  n   n")
+print ("p          y         t      h   h   o    o  n   n")
+print ("p          y         t      h   h   o    o  n   n")
+print ("p          y         t      h   h    oooo   n   n")
+# comments obaida 99067204      
